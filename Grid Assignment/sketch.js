ec043f495fc057c431b4718d80e778615aca0890
assiyab <160178060+assiyab@users.noreply.github.com>
2024-03-06 15:19:55 -0600

    Grid Assignment
    Day 2 of working on this project

--- Grid Assignment/sketch.js
+++ Grid Assignment/sketch.js
@@ -11,42 +11,53 @@ let squareSize = 25;
 
 function setup() {
   createCanvas(500, 500);
-  //x = width/2
-  //y = height/2
+  x = width/2
+  y = height/2
   document.addEventListener("contextmenu", event => event.preventDefault())
-  noLoop();
+  colourGrid();
   redraw();
   colorMode(RGB, 150);
   
 }
 
-function draw() {
+//function draw() {
   //draws columns and rows that will be spaced apart to make squares
+  //for( let rectWidth = 0; x > rectWidth; rectWidth++){
+    //for( let rectHeight = 0; y > rectHeight; rectHeight++){
+     // fill(random(255), random(255), random(255));
+      //rect(rectWidth * squareSize, rectHeight * squareSize, squareSize)
+      
+    //}  
+ // }
+  //noLoop();
+//}
+function mousePressed(){
+  if(mouseButton === RIGHT){
+    squareSize = squareSize / 0.5;  
+  }     
+
+  else if(mouseButton === LEFT){
+    squareSize = squareSize * 0.5 ;
+  }
+
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+    fill(random(255), random(255), random(255));
+  } 
+  
+}
+
+function colourGrid(){
   for( let rectWidth = 0; x > rectWidth; rectWidth++){
     for( let rectHeight = 0; y > rectHeight; rectHeight++){
       fill(random(255), random(255), random(255));
       rect(rectWidth * squareSize, rectHeight * squareSize, squareSize)
-      
+
     }
-  }
-  noLoop();
+  } 
+
 }
 
 
-function mousePressed(){
-  redraw();
-  fill(random(255), random(255), random(255));
-  if(mouseButton === RIGHT){
-    squareSize = squareSize * 0.5;  
-  }     
-
-  else if(mouseButton === LEFT){
-    squareSize = squareSize/ 0.5;
-  }
-}
-
-function keyPressed(){
-  redraw();
-  fill(random(255), random(255), random(255));
-  
-}
