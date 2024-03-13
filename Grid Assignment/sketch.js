4a7d866554cdeb10d01c983b907cc618fad786c9
assiyab <160178060+assiyab@users.noreply.github.com>
2024-03-13 14:19:34 -0600

    Grid Project
    Last day of working on grid project

--- Grid Assignment/sketch.js
+++ Grid Assignment/sketch.js
@@ -6,16 +6,17 @@
 let x;
 let y;
 let spacing = 30;
-let squareSize = 25;
+let squareSize = 20;
 
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(511, 511);
   document.addEventListener("contextmenu", event => event.preventDefault())
-  // no loop and deraw have to be enabled so that the screen is properly refreshed 
-  // and displaying current actions based on user interaction
+  background(255);
   colourGrid();
 }
+
+
 
 
 //what darws the gird, called in setup
@@ -23,36 +24,40 @@ function colourGrid() {
   // x and y act as columns and rows across srawing 25 sized squares 
   for (let x = 0; x < width; x += squareSize) {
     for (let y = 0; y < height; y += squareSize) {
-      fill(random(204, 255), random(153, 255), random(170, 255));
-      rect(x, y, squareSize);
-
+      //makes squares fit into grid no matter what size canvas is 
+      if (x + squareSize <= width && y + squareSize <= height) {
+        //fills random bute certain tones of colours
+        fill(random(204, 255), random(153, 255),random(170, 255));
+        rect(x, y, squareSize, squareSize);
+      }  
     }
   }
+    
 }
+
 
 
 // if right mouse click - bigger
 // if left mouse click - smaller
 function mousePressed() {
   if (mouseButton === RIGHT) {
-    //stops before falling off gris
-    if( squareSize < 200){
-      //square gets larger with every right click
+    //before square gets too big and falls off canvas
+    if (squareSize < 100) {
       squareSize = squareSize / 0.5;
+      background(255);
       colourGrid();
-    }
-
+    }  
   }
   
+
   else if (mouseButton === LEFT) {
-    if(squareSize > 6.25) {
-      //square gets smaller with each left click
+    // before squares get too small 
+    if (squareSize > 6.25) {
       squareSize = squareSize * 0.5;
+      background(255);
       colourGrid();
     }
-    
-  }    
-    
+  }      
 }
 
 // if the space key pressed, change the colour/order of the grid
@@ -61,4 +66,3 @@ function keyPressed() {
     colourGrid();
   }
 }
-
