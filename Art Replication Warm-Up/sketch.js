e0d7267ae1c37048c8b6b57db042f39ee0d5f1b0
assiyab <160178060+assiyab@users.noreply.github.com>
2024-03-27 15:22:36 -0600

    Replication Art Challenge
    Wednesday, March 27, 2024 day one of working on this replication generative art

--- Art Replication Warm-Up/sketch.js
+++ Art Replication Warm-Up/sketch.js
@@ -1,15 +1,39 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Replication Generative Art 
+// Assiya Boulfiza
+// March 27, 2024
+// Replication of computer composition with lines by Micheal Noll
+
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  background(220);
+  
+  for (let i = 0; i < 800; i++) {
+    // make sure they do not draw too far and keep the shape of a circle 
+    let x = random(width);
+    let y = random(height);
+    let squareSize = random(7, 10);
+    
+    if (dist(x, y, width / 2, height / 2) < 200) {
+      //circle will be drawn here, meausrments come from previous height and width from above
+      // distance measures the space between x and y points to make sure they stay within the circle shape
+      //Squares will only be drawn if they fall within the correct distance away from the center of the canvas
+      if (random() > 0.6) {
+        fill(0);
+        rect(x, y, squareSize + 3, squareSize - 7);
+      }else if (random() > 0.6){
+        fill(0);
+        rect(x, y, squareSize - 7 , squareSize + 3);
+      }
+      else if(random() > 0.2){
+        fill(0);
+        square(x, y, 5);
+      }
+      else{
+        rect(x, y, squareSize - 7 , squareSize + 3);
+      }
+    }
+  }
 }
 
-function draw() {
-  background(220);
-}
