7ca8721560178363ffbebebd5707cf701cc06b45
assiyab <160178060+assiyab@users.noreply.github.com>
2024-04-12 15:27:58 -0600

    First Day of Cars Assignment
    Friday, April 12, 2024 first day working on this assignment

--- Cars Cars Cars!/sketch.js
+++ Cars Cars Cars!/sketch.js
@@ -1,10 +1,7 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Cars Cars Cars!
+// Assiya Boulfiza
+// April 11, 2024
+// using classes and arrays and calling objects in different arrays.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +9,28 @@ function setup() {
 
 function draw() {
   background(220);
+  drawRoad();
+}
+
+function drawRoad(){
+  rectMode = (CENTER);
+  fill(0);
+  rect(0, height*0.25, width*2, 500)
+  while( i = 0, i <width, i ++){
+    fill(220);
+    rect(0, height*0.5, width*2, 10);
+    rect += 10;
+  }
+}
+
+class Vehicle{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.xspeed = 2;
+  }
+
+  display(){
+
+  }
 }
