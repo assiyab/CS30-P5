a62d61ef3f9e004f062b0b27b1fad7057e410c3f
assiyab <160178060+assiyab@users.noreply.github.com>
2024-04-25 13:02:07 -0600

    Puzzle Game
    Thursday, April 25, 2024
    Continuing to work on features for this puzzle game

--- 14 Puzzle Game Starter/sketch.js
+++ 14 Puzzle Game Starter/sketch.js
@@ -3,6 +3,8 @@
 // April 23, 2024
 // 
 
+let pattern = "cross";
+
 let grid = 
 [   [0,   255,  0,  255,  0],
     [0,   0,   255,  0,   0],
@@ -19,6 +21,8 @@ let col;
 
 function setup() {
   createCanvas(NUM_COLS*squareSize, NUM_ROWS*squareSize);
+  //keyPressed();
+  randomGrid();
 }
 
 function draw() {
@@ -28,8 +32,13 @@ function draw() {
   background(220);
   drawGrid();
   getCurrentX();
+  checkWinCondition();
 }
 
+function flip(x, y){
+  if(grid[y][x] === 0) grid[y][x] = 255;
+  else grid[y][x] = 0;
+}
 
 function mousePressed(){
  
@@ -50,23 +59,19 @@ function mousePressed(){
 
   //depends a bit on position: flip 4 neighbours
   //if(row < NUM_ROWS - 1) flip(col, row + 1); //top
-}
-
-function flip(x, y){
-  if(grid[y][x] === 0) grid[y][x] = 255;
-  else grid[y][x] = 0;
-}
-
-function getCurrentY(){
-  //determine current row of mouse and return it
-  let constrainY = constrain(mouseY, 0, height-1);
-  return int(constrainY/squareSize)
+  
 }
 
 function getCurrentX(){
   //detrmine current column mouse is in and return it 
   let constrainX = constrain(mouseX, 0, width-1);
   return int(constrainX/squareSize)
+}
+
+function getCurrentY(){
+  //determine current row of mouse and return it
+  let constrainY = constrain(mouseY, 0, height-1);
+  return int(constrainY/squareSize)
 }
 
 function drawGrid(){
@@ -76,7 +81,65 @@ function drawGrid(){
     for(let x = 0; x < NUM_COLS; x++){
       let fillValue = grid[y][x];
       fill(fillValue);
-      square(x*squareSize, y*squareSize, squareSize);
+      
+      // if (pattern === "cross") {
+      //   if (x === col || y === row) {
+      //     fill(0, 255, 0); 
+      //   } else {
+      //     fill(fillValue);
+      //   }
+      // } else if (pattern === "square") {
+      //   if (abs(x - col) <= 1 && abs(y - row) <= 1) {
+      //     fill(0, 255, 0);
+      //   } else {
+      //     fill(fillValue);
+      //   }
+      // } else {
+      //   fill(fillValue);
+      // }
+      square(x * squareSize, y * squareSize, squareSize);
     }
   }
 }
+
+
+function checkWinCondition() {
+  let allZeroes = false;
+  let allOnes = false;
+
+  for(let y = 0; y < NUM_ROWS; y++){
+    for(let x = 0; x < NUM_COLS; x++){
+      if(grid[y][x] === 0) {
+        allZeroes = true;
+      }
+      if(grid[y][x] === 255) {
+        allOnes = true;
+      }
+      
+    }
+    
+  }
+  
+  if(allZeroes || allOnes === grid[y][x]) {
+        print("You Win");
+    }
+}
+
+
+function randomGrid() {
+  for (let y = 0; y < NUM_ROWS; y++) {
+    for (let x = 0; x < NUM_COLS; x++) {
+      grid[y][x] = random(grid[x][y]);
+    }
+  }
+}
+
+// function keyPressed() {
+//   if (key === " ") {
+//     if (pattern === "cross") {
+//       pattern = "square";
+//     } else {
+//       pattern = "cross";
+//     }
+//   }
+// }
