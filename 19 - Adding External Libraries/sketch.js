cf5408878745da1018c5e8403e2c516573991ce0
assiyab <1000022@spsd.sk.ca>
2024-05-10 12:06:11 -0600

    Demo day
    Friday, May 10, demo on adding external libraries

---
+++ 19 - Adding External Libraries/sketch.js
@@ -0,0 +1,20 @@
+// Sample using external libraries
+// Assiya Boulfiza
+// May 10, 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let scribble = new Scribble();          
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  strokeWeight(4);
+  scribble.scribbleLine(width/2, height/2, mouseX, mouseY);
+  let radius = dist(width/2, height/2, mouseX, mouseY);
+  scribble.scribbleEllipse(width/2, height/2, radius*2, radius*2);
+}
