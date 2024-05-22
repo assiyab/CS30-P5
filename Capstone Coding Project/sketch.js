c208505f52d5f6214e7c49282a4398a77966ea2e
assiyab <1000022@spsd.sk.ca>
2024-05-22 15:25:55 -0600

    May 22
    making buttons and adding other features

--- Capstone Coding Project/sketch.js
+++ Capstone Coding Project/sketch.js
@@ -20,6 +20,12 @@
 
 // console.log(quote);
 
+let table;
+let afinn = {};
+
+function preload(){
+  table = loadTable('AFINN-111.txt', 'tsv');
+}
 
 let stars = [];
 let stickyNote = { active: false, x: 0, y: 0 };
@@ -27,6 +33,17 @@ let stickyNote = { active: false, x: 0, y: 0 };
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  console.log(table);
+  for(let i = 0; i < table.getRowCount(); i ++){
+    let row = table.getRow(i);
+    let word = row.get(0);
+    let score = row.get(1);
+
+    afinn[word] = score;
+  }
+
+  console.log(afinn);
+  //save(afinn, 'affinb11.json');
 
   for (let i = 0; i < 50; i++) {
     let x = random(width);
@@ -53,7 +70,7 @@ function draw() {
 function mouseClicked() {
   for (let star of stars) {
     let d = dist(mouseX, mouseY, star.x, star.y);
-    if (d < star.size / 2) { 
+    if (d < star.size / 2) { //possible bug/error
       stickyNote.active = true;
       stickyNote.x = star.x;
       stickyNote.y = star.y;
@@ -68,11 +85,49 @@ function keyPressed() {
     }
   }
 }
-
 function showStickyNote() {
+  //let noteWidth = width - stickyNote.x - 10; //  width dynamically
+  //let noteHeight = height - stickyNote.y - 10; // height dynamically
+
   fill('#FFC0CB');
-  rect(stickyNote.x, stickyNote.y, 150, 100, 10);
+  rect(0, 0, windowWidth, windowHeight);
   fill(0);
   textSize(16);
-  text("Submit your commit of the day", stickyNote.x + 10, stickyNote.y + 30); 
+  createButtons();
+  //text("Submit your commit of the day", stickyNote.x + 10, stickyNote.y + 30); 
 }
+
+function mouseIsPressed(){
+  if(createButtons === "true"){
+    if(journalBtn === "true"){
+      journal();
+    }
+    else{
+      quoteGenerator();
+    }
+  }
+}
+
+function createButtons() {
+  let journalBtn = createButton('Journal For the Day');
+  journalBtn.position(430, height - 100);
+  journalBtn.style('background-color', '#C8A2C8');
+  journalBtn.style('border-radius', '10px');
+  journalBtn.style('font-size', '16px');
+
+  let quoteBtn = createButton('Generate Your Quote of the Day!');
+  quoteBtn.position(10, height - 100);
+  quoteBtn.style('background-color', '#C8A2C8');
+  quoteBtn.style('border-radius', '10px');
+  quoteBtn.style('font-size', '16px');
+  };
+
+  function journal(){
+    showStickyNote();
+    rect(0, 0, 300, 300);
+    fill("white");
+
+
+  }
+
+  
